2edb567f5ae2ba2808a47a8267e2456e9a527ac1
Will Gorham <willgorham@gmail.com>
2018-09-26 18:38:30 -0400

    Add handling of product meta

--- includes/class-wcpt-admin.php
+++ includes/class-wcpt-admin.php
@@ -5,3 +5,48 @@
  * @package WooCommerce Product Tagline
  * @since   1.0.0
  */
+
+defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
+
+/**
+ * Admin and settings support.
+ *
+ * @version 1.0.0
+ */
+class WCPT_Admin {
+
+  /**
+   * Initialize class.
+   */
+  public static function init() {
+    add_action( 'woocommerce_product_options_general_product_data', array( __CLASS__, 'add_tagline_field' ) );
+    add_action( 'woocommerce_process_product_meta', array( __CLASS__, 'save_tagline_field' ) );
+  }
+
+  /**
+   * Output custom tagline text field.
+   */
+  public static function add_tagline_field() {
+    woocommerce_wp_textarea_input( array(
+      'id' => '_tagline',
+      'label' => 'Product Tagline',
+      'description' => 'This tagline will be displayed in product listings (e.g. the Shop page).',
+      'desc_tip' => 'true',
+      'placeholder' => ''
+    ) );
+  }
+
+  /**
+   * Save tagline field data.
+   *
+   * @param int $post_id Post ID
+   */
+  public static function save_tagline_field( $post_id ) {
+    if ( ! empty( $_POST['_tagline'] ) ) {
+        update_post_meta( $post_id, '_tagline', sanitize_textarea_field( $_POST['_tagline'] ) );
+    }
+  }
+
+}
+
+WCPT_Admin::init();
