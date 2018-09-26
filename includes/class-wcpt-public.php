6ca238f936f0f243aa047dc65b8bf6c44f613b28
Will Gorham <willgorham@gmail.com>
2018-09-26 18:46:41 -0400

    Output tagline content on product listing

--- includes/class-wcpt-public.php
+++ includes/class-wcpt-public.php
@@ -5,3 +5,35 @@
  * @package WooCommerce Product Tagline
  * @since   1.0.0
  */
+
+defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
+
+/**
+ * Admin and settings support.
+ *
+ * @version 1.0.0
+ */
+class WCPT_Admin {
+
+  /**
+   * Initialize class.
+   */
+  public static function init() {
+    add_action( 'woocommerce_shop_loop_item_title', array( __CLASS__, 'output_tagline' ), 20 );
+  }
+
+  /**
+   * Print tagline field content.
+   */
+  public static function output_tagline() {
+    $tagline_content = get_post_meta( get_the_ID(), '_tagline', true );
+
+    echo '<div class="product-tagline"><em>';
+    echo esc_html( $tagline_content );
+    echo '</em></div>';
+  }
+
+
+}
+
+WCPT_Admin::init();
