e7f8614be024b4ff72737db88ea43a766140fbee
Will Gorham <willgorham@gmail.com>
2018-09-26 18:00:45 -0400

    Class skeleton

---
+++ woocommerce-product-tagline.php
@@ -0,0 +1,85 @@
+<?php
+/*
+Plugin Name: WooCommerce Product Tagline
+Plugin URI: https://willgorham.com/
+Description: Add a short product tagline to WooCommerce products with display options.
+Version: 1.0.0
+Author: Will Gorham
+Author URI: https://willgorham.com/
+Copyright: Will Gorham
+*/
+
+defined( 'ABSPATH' ) || exit; // Exit if accessed directly
+
+class WCPT {
+
+  /* Plugin version. */
+  const VERSION = '1.0.0';
+
+  /**
+   * @var WCPT - the single instance of the class.
+   *
+   * @since 1.0.0
+   */
+  protected static $instance = null;
+
+  /**
+   * Cloning is forbidden.
+   *
+   * @since 1.0.0
+   */
+  protected function __clone() {
+    _doing_it_wrong( __FUNCTION__, 'Nah.', '1.0.0' );
+  }
+
+  /**
+   * Unserializing instances of this class is forbidden.
+   *
+   * @since 1.0.0
+   */
+  protected function __wakeup() {
+    _doing_it_wrong( __FUNCTION__, 'Nah.', '1.0.0' );
+  }
+
+  /**
+   * Do some work.
+   */
+  private function __construct() {
+
+  }
+
+  /**
+   * Main WCPT Instance.
+   *
+   * Ensures only one instance of WCPT is loaded or can be loaded.
+   *
+   * @static
+   * @see wcpt()
+   * @return WCPT - Main instance
+   * @since 1.0.0
+   */
+  public static function getInstance() {
+
+    if ( is_null( self::$instance ) ) {
+      self::$instance = new self();
+    }
+
+    return self::$instance;
+  }
+
+}
+
+
+/**
+ *  Returns the singleton WCPT object.
+*
+*  @since   1.0.0
+*  @return  WCPT
+*/
+function wcpt() {
+  return WCPT::getInstance();
+}
+
+
+// Initialize this thing.
+$GLOBALS[ 'wcpt' ] = wcpt();
